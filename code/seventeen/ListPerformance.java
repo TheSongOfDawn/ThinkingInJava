9554df99d9e03847a8a41f1a27849fb3dc60ebc0
lk <405099871@qq.com>
2017-08-02 17:55:44 +0800

    17

---
+++ code/seventeen/ListPerformance.java
@@ -0,0 +1,16 @@
+//17.10.2¶ÔlistµÄÑ¡Ôñ
+package seventeen;
+
+import java.util.List;
+import java.util.Random;
+
+public class ListPerformance {
+	static Random rand=new Random();
+	static int reps=1000;
+	static List<Test<List<Integer>>> test=
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
